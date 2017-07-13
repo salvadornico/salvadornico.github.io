0dc6641538e9fadf9a461a54244b06259eae36d7
Nico Salvador <salvador.nico@gmail.com>
2017-07-13 09:39:15 +0800

    Got ScrollFire working properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,63 @@
 $(document).ready( function() {
+
 	var contacts = document.getElementById("contacts")
 	var contactsHeight = $(window).height() - $("footer").height()
 	contacts.style.height = contactsHeight + "px"
 	
-	var anchor = document.querySelector("#contacts")
+	var contactsAnchor = document.querySelector("#contacts")
 	var toggle = document.querySelector("#scrollDownBtn")
-	var options = {
+	var smoothScrollOptions = {
 		speed: 1400,
 		easing: "easeOutQuint",
-		after: function (anchor, toggle) {
-			$("#scrollToTopFAB").addClass("scale-in pulse")
-			setTimeout( function(){
-				$("#scrollToTopFAB").removeClass("scale-out scale-in")
-			}, 500)
-		}
+		after: function (contactsAnchor, toggle) { popOutFAB() }
 	}
 	$("#scrollDownBtn").click( function() {
-		smoothScroll.animateScroll(anchor, toggle, options)
+		smoothScroll.animateScroll(contactsAnchor, toggle, smoothScrollOptions)
 	})
 
 	$("#scrollToTopFAB").click( function() {
-		var anchor = document.querySelector("#banner")
-		smoothScroll.animateScroll(anchor)
+		var bannerAnchor = document.querySelector("#banner")
+		smoothScroll.animateScroll(bannerAnchor)
 		$("#scrollToTopFAB").addClass("scale-out")
 		$("#scrollToTopFAB").removeClass("pulse")
 	})
 
-	$('.parallax').parallax()
+	$(".parallax").parallax()
+
+	var scrollFireOptions = [
+		{selector: "#collapsible-1", offset: 250, callback: function() {
+			$(".collapsible").collapsible("open", 0)
+		}},
+		{selector: "#collapsible-2", offset: 250, callback: function() {
+			$(".collapsible").collapsible("open", 1)
+			popOutFAB()
+		}},
+		{selector: "#collapsible-3", offset: 250, callback: function() {
+			$(".collapsible").collapsible("open", 2)
+		}},
+		{selector: "#collapsible-4", offset: 250, callback: function() {
+			$(".collapsible").collapsible("open", 3)
+		}},
+	]
+	Materialize.scrollFire(scrollFireOptions)
 	
-	var thingBox = document.getElementById('thingBox')
-	for (i = 0; i < thingsList.length; ++i) {    
+	var thingBox = document.getElementById("thingBox")
+	for (i = 0; i < thingsList.length; ++i) {
 		thingBox.innerHTML += '<div class="thing col s6 m4 l3"><a href="' + 
 			thingsList[i][0] + '"><div class="card small hoverable"><div class="card-image"><img src="images/things/' + 
 			thingsList[i][1] + '" alt ="' + thingsList[i][2] + '"></div><div class="card-content valign-wrapper"><h5 class="black-text center-align">' + thingsList[i][2] + '</h5></div></div></a></div>'
 	}
 })
+
+function popOutFAB() {
+	$("#scrollToTopFAB").addClass("scale-in pulse")
+	setTimeout( function() {
+		$("#scrollToTopFAB").removeClass("scale-out scale-in")
+	}, 500)
+	setTimeout( function() {
+		$("#scrollToTopFAB").removeClass("pulse")
+	}, 10000)
+}
 
 var thingsList = [
 	["https://www.tested.com", "tested-logo.png", "Tested.com"],
