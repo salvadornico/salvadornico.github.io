b9e5a34f8b9a97dee825e329f4cd135be1656eaa
Nico Salvador <salvador.nico@gmail.com>
2017-07-23 13:08:49 +0800

    Added tech skill logos

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,3 +1,16 @@
+var techSkills = [
+	["HTML5", "html5.png"],
+	["CSS3", "css3.png"],
+	["Javascript", "JS.png"],
+	["jQuery", "jquery.png"],
+	["PHP", "php.png"],
+	["Laravel", "laravel.png"],
+	["Java", "java.png"],
+	["Android", "android.png"],
+	["Kotlin", "kotlin.png"],
+	["Python", "python.png"],
+]
+
 var thingsList = [
 	["https://www.tested.com", "tested-logo.png", "Tested.com"],
 	["http://www.garthstein.com/works/the-art-of-racing-in-the-rain", "racing_in_the_rain.jpg", "The Art of Racing in the Rain"],
@@ -6,7 +19,7 @@ var thingsList = [
 	["https://www.theminimalists.com/etr", "etr.jpg", "Everything That Remains"],
 	["http://www.switchedonpop.com", "switched-on-pop.png", "Switched On Pop"],
 	["http://www.codemasters.com/game/dirt-4", "DiRT4.png", "DiRT 4"],
-	["http://www.valo4life.com/", "Valo4life.jpg", "Valo 4 Life"]
+	["http://www.valo4life.com/", "Valo4life.jpg", "Valo 4 Life"],
 ]
 
 $(document).ready( function() {
@@ -58,6 +71,14 @@ $(document).ready( function() {
 			thingsList[i][0] + '"><div class="card small hoverable"><div class="card-image"><img src="images/things/' + 
 			thingsList[i][1] + '" alt ="' + thingsList[i][2] + '"></div><div class="card-content valign-wrapper"><h5 class="black-text center-align">' + thingsList[i][2] + '</h5></div></div></a></div>'
 	}
+
+	var skillsList = document.getElementById("skillsList")
+	for (i = 0; i < techSkills.length; ++i) {
+		skillsList.innerHTML += '<img src="images/logos/' + techSkills[i][1] + 
+			'" alt="' + techSkills[i][0] + '" class="tech-skill tooltipped" data-position="bottom" data-delay="50" data-tooltip="' + techSkills[i][0] + '">'
+	}
+	$('.tooltipped').tooltip({delay: 50})
+
 })
 
 function popOutFAB() {
