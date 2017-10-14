bbaf6d5139f50186d2659a5c5af3971696fc717b
Nico <salvador.nico@gmail.com>
2017-10-15 01:30:07 +0800

    Set up ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,25 @@
 // http://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  env: {
-    browser: true,
-  },
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  'rules': {
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'arrow-parens': 0,
-    'generator-star-spacing': 0,
-    'indent': ['warn', 'tab']
-  }
+	root: true,
+	parser: "babel-eslint",
+	parserOptions: {
+		sourceType: "module"
+	},
+	env: {
+		browser: true,
+	},
+	// required to lint *.vue files
+	plugins: [
+		"html"
+	],
+	"rules": {
+		"no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
+		"arrow-parens": 0,
+		"generator-star-spacing": 0,
+		"indent": ["error", "tab"],
+		"quotes": ["error", "double"],
+		"linebreak-style": ["error", "unix"],
+		"no-undef": "off"
+	}
 }
