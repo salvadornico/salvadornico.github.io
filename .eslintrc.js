ff44a626c115f45177f95d5038237a7b764a883c
Nico <salvador.nico@gmail.com>
2017-10-15 00:35:44 +0800

    Finished setup of Vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-// https://eslint.org/docs/user-guide/configuring
+// http://eslint.org/docs/user-guide/configuring
 
 module.exports = {
   root: true,
@@ -13,9 +13,10 @@ module.exports = {
   plugins: [
     'html'
   ],
-  // add your custom rules here
   'rules': {
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'arrow-parens': 0,
+    'generator-star-spacing': 0,
+    'indent': ['warn', 'tab']
   }
 }
--- README.md
+++ README.md
@@ -5,9 +5,6 @@
 ## Build Setup
 
 ``` bash
-# install dependencies
-npm install
-
 # serve with hot reload at localhost:8080
 npm run dev
 
@@ -16,6 +13,8 @@ npm run build
 
 # build for production and view the bundle analyzer report
 npm run build --report
-```
 
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+# To start Flask back-end:
+export FLASK_DEBUG=1 && export FLASK_APP=app.py
+flask run
+```
