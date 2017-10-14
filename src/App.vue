e6342216195a9de6fddca53a74e1f917f8f05fae
Nico <salvador.nico@gmail.com>
2017-10-15 01:07:23 +0800

    Set up Pug and Stylus in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
-<template>
-	<div id="app">
-		<img src="./assets/logo.png">
-		<h1> helloo</h1>
-		<router-view/>
-	</div>
+<template lang="pug">
+	#app
+		img(src="./assets/logo.png")
+		h1 helloo
+		router-view
 </template>
 
 <script>
@@ -12,13 +11,15 @@ export default {
 }
 </script>
 
-<style>
-#app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-}
+<style lang="stylus">
+#app
+	font-family "Avenir", Helvetica, Arial, sans-serif
+	-webkit-font-smoothing antialiased
+	-moz-osx-font-smoothing grayscale
+	text-align center
+	color #2c3e50
+	margin-top 60px
+
+	h1
+		color blue
 </style>
