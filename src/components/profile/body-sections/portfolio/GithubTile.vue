0bd7b349bd119e13ceffbc0624adf4d05257cc51
Nico <salvador.nico@gmail.com>
2018-02-05 22:18:29 +0800

    Laid out tiles as grid

---
+++ src/components/profile/body-sections/portfolio/GithubTile.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+.github-tile.card.grey
+	h5 Latest Github Activity
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+
+export default {
+	computed: { ...mapGetters(["projects"]) },
+}
+</script>
+
+<style lang="stylus" scoped>
+.github-tile
+	padding 1rem
+</style>
--- src/components/profile/body-sections/portfolio/Portfolio.vue
+++ src/components/profile/body-sections/portfolio/Portfolio.vue
@@ -5,25 +5,28 @@
 	.card
 		.card-content
 			span.card-title Portfolio
-			.row.portfolio-item(v-for="project in projects")
-				.col.m4
+			.portfolio
+				GithubTile.portfolio-item
+				.portfolio-item.portfolio-link(v-for="project in projects")
 					img.responsive-img(v-bind:src="imgLink(project.image)")
-				.col.m8
-					a(v-bind:href="project.url")
-						h4 {{ project.title }}
-					p {{ project.description }}
+					.portfolio-item-description
+						a(v-bind:href="project.url")
+							h4 {{ project.title }}
+						p {{ project.description }}
 	BackButton
 	br
 </template>
 
 <script>
 import BackButton from "@/components/shared/BackButton"
+import GithubTile from "@/components/profile/body-sections/portfolio/GithubTile"
 import imageService from "@/helpers/imageService.js"
 import { mapGetters } from "vuex"
 
 export default {
 	components: {
 		BackButton,
+		GithubTile,
 	},
 	computed: { ...mapGetters(["projects"]) },
 	methods: {
@@ -31,3 +34,26 @@ export default {
 	},
 }
 </script>
+
+<style lang="stylus" scoped>
+.card-title
+	font-weight bold
+
+.portfolio
+	display grid
+	grid-template-columns repeat(auto-fit, minmax(10rem, 1fr))
+	grid-gap 1rem
+	grid-autoflow dense
+	.portfolio-item
+		&:hover
+			grid-column span 3
+	.portfolio-link
+		.portfolio-item-description
+			display none
+		&:hover
+			display grid
+			grid-template-columns 1fr 1fr
+			grid-gap 1rem
+			.portfolio-item-description
+				display block
+</style>
