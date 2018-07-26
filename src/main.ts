3ce19215d4efb8175ab61d7d2fb2694bb3442995
Nico Salvador <salvador.nico@gmail.com>
2018-07-26 22:20:13 +0800

    Added vue-analytics

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import router from "@/router"
 import store from "@/store/index"
 import Vue from "vue"
+import VueAnalytics from "vue-analytics"
 import App from "./App.vue"
+import config from "./config"
 
 Vue.config.productionTip = false
 Vue.prototype.$eventBus = new Vue()
+
+Vue.use(VueAnalytics, {
+	id: config.analyticsId,
+	router,
+	debug: {
+		sendHitTask: process.env.NODE_ENV === "production",
+	},
+})
 
 new Vue({
 	router,
