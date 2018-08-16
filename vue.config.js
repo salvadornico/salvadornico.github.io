064f46ec130350e21cf24cf57c2e38ffe788ba97
Nico Salvador <salvador.nico@gmail.com>
2018-08-16 21:20:59 +0800

    Added prerender-spa-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,15 @@
 const path = require("path")
+const PrerenderSPAPlugin = require("prerender-spa-plugin")
 
 module.exports = {
+	configureWebpack: {
+		plugins: [
+			new PrerenderSPAPlugin({
+				staticDir: path.join(__dirname, "dist"),
+				routes: ["/", "/skills"],
+			}),
+		],
+	},
 	chainWebpack: config => {
 		config.module
 			.rule("vue")
