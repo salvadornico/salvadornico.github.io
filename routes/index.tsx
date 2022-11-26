85d42737bf4e5dab72ad7e368287c86d2f4b3447
Nico Salvador <salvador.nico@gmail.com>
2022-11-26 20:24:16 +0800

    Base card layout

--- routes/index.tsx
+++ routes/index.tsx
@@ -13,38 +13,48 @@ export default function Home() {
           rel="shortcut icon"
           type="image/png"
           href="https://res.cloudinary.com/salvadornico/image/upload/v1507347645/Portfolio/icon.png"
-        >
-        </link>
+        />
         <link
           href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans"
           rel="stylesheet"
-        >
-        </link>
+        />
+        <link
+          href="css/base.css"
+          rel="stylesheet"
+        />
       </Head>
 
       <main className="bg-blue-800">
-        <div class="p-4 mx-auto max-w-screen-md">
-          <h1 class="text-2xl mb-3">Nico Salvador</h1>
-          <h3>Mandaluyong, Philippines 🇵🇭</h3>
-          <span class="text-xs text-slate-300">(current)</span>
-          <h3>Quezon City, Philippines 🇵🇭</h3>
-          <span class="text-xs text-slate-300">(from)</span>
-          <div className="link-icons mt-3">
-            <a class="inline-block" href={"mailto:salvador.nico@gmail.com"}>
-              <IconMail class="w-6 h-6" />
-            </a>
-            <a class="inline-block" href="https://github.com/salvadornico">
-              <IconBrandGithub class="w-6 h-6" />
-            </a>
-            <a
-              class="inline-block"
-              href="https://www.linkedin.com/in/salvadornico"
-            >
-              <IconBrandLinkedin class="w-6 h-6" />
-            </a>
+        <div className="card w-3/5">
+          <Image file="portrait.png" alt="That's me!" />
+          <div className="bg-gray-100 p-4">
+            <h1 className="text-2xl mb-3">Nico Salvador</h1>
+            <h3>Mandaluyong, Philippines 🇵🇭</h3>
+            <span className="text-xs text-slate-300">(current)</span>
+            <h3>Quezon City, Philippines 🇵🇭</h3>
+            <span className="text-xs text-slate-300">(from)</span>
+            <div className="link-icons mt-3">
+              <a
+                className="inline-block"
+                href={"mailto:salvador.nico@gmail.com"}
+              >
+                <IconMail className="w-6 h-6" />
+              </a>
+              <a
+                className="inline-block"
+                href="https://github.com/salvadornico"
+              >
+                <IconBrandGithub className="w-6 h-6" />
+              </a>
+              <a
+                className="inline-block"
+                href="https://www.linkedin.com/in/salvadornico"
+              >
+                <IconBrandLinkedin className="w-6 h-6" />
+              </a>
+            </div>
           </div>
         </div>
-        <Image file="portrait.png" alt="That's me!" />
       </main>
     </>
   );
