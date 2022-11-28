843194ad94826dc60417bec31ba48ee92f17b241
Nico Salvador <salvador.nico@gmail.com>
2022-11-28 08:10:43 +0800

    More card content

--- islands/CardSection.tsx
+++ islands/CardSection.tsx
@@ -2,7 +2,7 @@ import { getUrl, ImageSize } from "../services/image.service.ts";
 import CollapsibleCard from "./CollapsibleCard.tsx";
 
 const getImageSection = (images: { file: string; alt: string }[]) => (
-  <div className="max-w-full text-center mb-2 mt-3">
+  <div className="max-w-full text-center m-2">
     {images.map(({ file, alt }) => (
       <img
         class="inline-block m-1"
@@ -24,7 +24,8 @@ export default function CardSection() {
       id="interest-cards"
       className="flex flex-col items-center gap-4 p-6"
     >
-      <CollapsibleCard open title="Making Things">
+      <h1 class="text-2xl text-white">Things I'm Into</h1>
+      <CollapsibleCard open title="Making">
         <div className="grid md:grid-cols-2">
           {getImageSection([
             {
@@ -80,6 +81,69 @@ export default function CardSection() {
           creatively and then the application of those ideas in a logical and
           systematic fashion.
         </p>
+      </CollapsibleCard>
+      <CollapsibleCard title="Inline Skating">
+        <div className="grid md:grid-cols-[repeat(2,auto)] gap-2 justify-items-center">
+          <div>
+            <p>
+              I've been at it on and off since 2002 (see my lame 2011-era video
+              footage to the side), skating parks all around Asia-Pacific along
+              the way.
+            </p>
+            <p>
+              Used to have a side project bringing in gear from shops all over
+              the world to the Philippines, but scale was difficult at the time
+              and so it's closed now. I also learned a lot about filming and
+              editing video from being involved in this scene.
+            </p>
+          </div>
+          <iframe
+            src="https://player.vimeo.com/video/31335546?portrait=0"
+            frameBorder="0"
+            allowFullScreen
+          />
+        </div>
+      </CollapsibleCard>
+      <CollapsibleCard title="Marketing/PR/events">
+        <p>
+          I graduated from the University of the Philippines with a degree in
+          Business Administration. And while most of my related experience
+          afterwards was in marketing, I'm perfectly comfortable discussing
+          economics, finance, corporate strategy, or management, and have even
+          been called up to draft a contract or two.
+        </p>
+        <p>
+          I've helped organize an{" "}
+          <a class="text-blue-700" href="https://youtu.be/hyKqQg_m8Ds">
+            indie-orchestra mashup concert
+          </a>,{" "}
+          <a
+            class="text-blue-700"
+            href="https://www.youtube.com/watch?v=iqqKuvqXKj4"
+          >
+            football tournaments
+          </a>, and an{" "}
+          <a
+            class="text-blue-700"
+            href="https://motioncars.inquirer.net/28757/name-kias-pba-team-and-win-a-trip-to-an-nba-game-in-los-angeles"
+          >
+            online contest to name a professional basketball team
+          </a>, among many other less interesting things.
+        </p>
+        <div class="m-2 text-center">
+          <iframe
+            class="inline-block m-1"
+            src="https://www.youtube.com/embed/hyKqQg_m8Ds"
+            frameBorder="0"
+            allowFullScreen
+          />
+          <iframe
+            class="inline-block m-1"
+            src="https://www.youtube.com/embed/iqqKuvqXKj4"
+            frameBorder="0"
+            allowFullScreen
+          />
+        </div>
       </CollapsibleCard>
     </div>
   );
